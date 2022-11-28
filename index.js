30572d6a9841957ebde484b710235248ade97c76
Savannah1304 <111804790+Savannah1304@users.noreply.github.com>
2022-11-28 16:31:05 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 document.querySelector("h1").addEventListener("click", function (){
 var pizza = Math.floor(Math.random()*16777215).toString(16);
-  document.body.style.color = "#"+Pizza; 
+  document.body.style.color = "#"+pizza; 
 });
